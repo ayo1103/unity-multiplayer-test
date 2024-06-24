dc383e1e9af208ba4cb7d728b50d683910fd0945
Ayo <ayo@metatheory.gg>
2024-06-24 23:10:49 +0800

    add reconnect button

--- Assets/#Test/Scripts/NetworkConnectionHelper.cs
+++ Assets/#Test/Scripts/NetworkConnectionHelper.cs
@@ -1,11 +1,21 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkConnectionHelper : MonoBehaviour
 {
+    [SerializeField] private Camera _offlineCamera;
+    [SerializeField] private Button _reconnectButton;
+
     public bool autoStartServer;
     public bool autoStartHost;
     public bool autoStartClient;
+
+    private void Awake()
+    {
+        _reconnectButton.gameObject.SetActive(false);
+    }
 
     public void Start()
     {
@@ -23,5 +33,48 @@ public class NetworkConnectionHelper : MonoBehaviour
         {
             NetworkManager.Singleton.StartClient();
         }
+        
+        NetworkManager.Singleton.OnConnectionEvent += OnConnectionEvent;
+    }
+
+    private void OnDestroy()
+    {
+        NetworkManager.Singleton.OnConnectionEvent -= OnConnectionEvent;
+    }
+
+    private void OnEnable()
+    {
+        _reconnectButton.onClick.AddListener(OnClickReconnectButton);
+    }
+
+    private void OnDisable()
+    {
+        _reconnectButton.onClick.RemoveListener(OnClickReconnectButton);
+    }
+
+    private void OnConnectionEvent(NetworkManager networkManager, ConnectionEventData connectionEventData)
+    {
+        if (connectionEventData.ClientId == NetworkManager.Singleton.LocalClientId)
+        {
+            Debug.Log($"connectionEventData = {connectionEventData.ClientId} event = {connectionEventData.EventType}");
+            switch (connectionEventData.EventType)
+            {
+                case ConnectionEvent.ClientDisconnected:
+                    _offlineCamera.enabled = true;
+                    _reconnectButton.gameObject.SetActive(true);
+                    break;
+
+                case ConnectionEvent.ClientConnected:
+                    _offlineCamera.enabled = false;
+                    _reconnectButton.gameObject.SetActive(false);
+                    break;
+            }
+        }
+    }
+
+    private void OnClickReconnectButton()
+    {
+        NetworkManager.Singleton.Shutdown();
+        NetworkManager.Singleton.StartClient();
     }
 }
