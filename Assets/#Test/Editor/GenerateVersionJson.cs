1bf2fbd36349f41c96dccb865c28f6bd0e9ae6ed
Ayo <ayo@metatheory.gg>
2024-06-24 19:40:53 +0800

    add dynamic check bundle version to prevent browser cache

---
+++ Assets/#Test/Editor/GenerateVersionJson.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class GenerateVersionJson : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        string version = Application.version;
+        string json = $"{{ \"version\": \"{version}\" }}";
+        string path = Path.Combine(report.summary.outputPath, "Build/version.json");
+
+        File.WriteAllText(path, json);
+        Debug.Log($"Generated version.json with version {version} at {path}");
+    }
+}
